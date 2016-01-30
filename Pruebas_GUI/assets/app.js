090dd92649b8eaf5865ebda0ceed4d737b866bf7
CarlosEd91 <carlos.e.sampedro.91@gmail.com>
2016-01-30 12:02:04 -0500

    Avanzando

--- Pruebas_GUI/assets/app.js
+++ Pruebas_GUI/assets/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('Pruebas',['ui.router','ngResource']);
+var app = angular.module('Pruebas',['ui.router','ngResource','toastr']);
 
 app.config(function($stateProvider,$urlRouterProvider){
     
--- Pruebas_GUI/assets/controladores/EleccionController.js
+++ Pruebas_GUI/assets/controladores/EleccionController.js
@@ -1,5 +1,16 @@
-app.controller('EleccionController',['$scope',function($scope){
-    
+app.controller('EleccionController', ['$scope', '$http',function($scope,$http) {
+
     console.log('Entraste a Eleccion');
+
+    $scope.pruebas;
+    $http({
+        method: 'GET',
+        url: 'http://localhost:1337/Pruebas'
+    }).then(function correcto(respuesta){
+        $scope.pruebas = respuesta.data;
+        console.log($scope.pruebas);
+    })
+    
+
 
 }]);
