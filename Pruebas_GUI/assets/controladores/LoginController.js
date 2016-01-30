096a3393ed7026b81b01be0b283b2e993c861bd6
Stan <stan.deuxie@live.com>
2016-01-30 11:52:11 -0500

    login por stalin controlador y html validado

--- Pruebas_GUI/assets/controladores/LoginController.js
+++ Pruebas_GUI/assets/controladores/LoginController.js
@@ -1,5 +1,29 @@
-app.controller('LoginController',['$scope',function($scope){
-    
-    console.log('Entraste a Login');
+app.controller('LoginController', ['$scope','$http', '$location', function ($scope,$http,$location) {
 
+    console.log('Entraste a Login');
+    
+    $scope.usuario;
+    $scope.correo;
+    $scope.password;
+    
+    $scope.validarUsuario = function validarUsuario() {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:1337/Usuarios/?correo='+$scope.correo+'&password='+$scope.password
+        }).then(function correcto(respuesta){
+                $scope.usuario=respuesta;
+            
+                console.log(respuesta.data);
+                if(respuesta.data.length<1){
+                    console.log('El usuario no existe');
+                }
+                else{
+                    $location.path("/eleccion");
+                }
+            
+        });
+    };
+    
+
+    
 }]);
